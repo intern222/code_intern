dad84550bdd21616fce1f1145b71bdf523228677
Diogo <dmartins@annscom.de>
2020-12-16 22:36:03 +0000

    updated backend

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,15 @@ import data from './data.js';
 
 const app = express();
 
+app.get('/api/internships/:id', (req, res) =>{
+    const internship = data.internships.find(x => x._id === req.params.id);
+    if(internship){
+        res.send(internship);
+    }else {
+        res.status(404).send({message:'Internship not found'});
+    }
+});
+
 app.get('/api/internships', (req, res) =>{
     res.send(data.internships);
 });
